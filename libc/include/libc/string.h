3d934c25adb7b9dc752f5b1d8f6cb236b637992a
Antonette Caldwell <aacaldwell42@outlook.com>
2021-06-08 15:41:01 -0500

    Removed the extra files

--- libc/include/libc/string.h
+++ libc/include/libc/string.h
@@ -1,23 +1,51 @@
-#ifndef ISMENAOS_LIBC_STRING_H
-#define ISMENAOS_LIBC_STRING_H 1
+/******************************************************************************
+BSD 3-Clause License
 
-#include "sys/cdefs.h"
+Copyright (c) 2021, Antonette Caldwell
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+********************************************************************************/
+
+#ifndef _ISMENAOS_LIBC_STRING_H
+#define _ISMENAOS_LIBC_STRING_H 1
+
+#include "types.h"
+#include <sys/cdefs.h>
 
 #include <stddef.h>
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
+__BEGIN_C_CODE;
 
-int memcmp(const void *, const void *, size_t);
-void *memcpy(void *__restrict, const void *__restrict, size_t);
-void *memmove(void *, const void *, size_t);
-void *memset(void *, int, size_t);
-size_t strlen(const char *);
+void *memcpy(void *, const void *, uint);
+int memcmp(const void *__restrict, const void *__restrict, uint);
+void *memmove(void *, const void *, uint);
+void *memset(void *, int, uint);
+uint strlen(const char *);
 
-#ifdef __cplusplus
-}
-#endif
+__END_C_CODE;
 
-#endif //ISMENAOS_LIBC_STRING_H
+#endif // _ISMENAOS_LIBC_STRING_H
